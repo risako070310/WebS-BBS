17eb40fad458aeaf3f3659024bdf942d32ae42f6
risako070310 <risako070310@gmail.com>
2022-04-28 11:39:07 +0000

    add form

--- app.rb
+++ app.rb
@@ -1,8 +1,24 @@
 require 'bundler/setup'
 Bundler.require
 require 'sinatra/reloader' if development?
+require 'open-uri'
+require 'sinatra/json'
+require './models/contribution.rb'
 
 before do
 
+end
 
+get '/' do
+    @contents = Contribution.all.order('id desc')
+    erb :index
+end
+
+post '/new' do
+    Contribution.create({
+        name: params[:user_name],
+        body: params[:body]
+    })
+    
+    redirect '/'
 end
